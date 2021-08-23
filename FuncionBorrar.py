76ee309d00318021de5f19465bea33a85ac74cf6
ImJumo <daniel.ayala621@gmail.com>
2021-08-23 13:42:30 -0500

    F_Borrar,F_Listar

---
+++ FuncionBorrar.py
@@ -0,0 +1,50 @@
+import pandas as pd
+import numpy as np
+
+#Cargar la BD.tx al array
+a = np.loadtxt('BD.txt',dtype=str)
+#Contar las filas que hay para el data frame
+e = 0
+f = 0
+for row in a:
+    for elem in row:
+        e = e+1
+    f = f+1
+row_labels = []
+cont = 1
+while cont < f+1:
+    row_labels.append(cont)
+    cont = cont+1
+column_labels = ['Nombre','Apellido']
+#Hacer un data DataFrame
+df = pd.DataFrame(a,columns=column_labels,index=row_labels)
+print(df)
+print("\n")
+match = int(input("Que fila quieres borrar?: "))
+
+
+#Borra la fila con indice x (Especificado por el usuario)
+print("\n")
+b = np.delete(a,match-1,axis=0)
+print("Fila borrada: "+ str(match)+"\n")
+#Volver a contar filas y mostrar nueva BD
+e=0#Inicializar e y f
+f=0
+for row in b:
+    for elem in row:
+        e = e+1
+    f = f+1
+row_labels = []
+cont = 1
+while cont < f+1:
+    row_labels.append(cont)
+    cont = cont+1
+column_labels = ['Nombre','Apellido']
+#Nuevo DataFrame de b
+print("Nueva Base de Datos: \n")
+dfb = pd.DataFrame(b,columns=column_labels,index=row_labels)
+print(dfb)
+print("\n")
+
+
+
---
+++ FuncionListar.py
@@ -0,0 +1,30 @@
+import numpy as np
+import pandas as pd
+a = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]
+    ]
+#Contar filas del array
+e = 0 #Elementos del array totales
+f = 0 #Filas del Array
+for row in a:
+    for elem in row:
+       e = e+1
+    f = f+1   
+#Establecer Columnas con un for
+row_labels = [] #Se empieza un array vacio para llenarlo con nueva info
+cont = 1 #Contador del while son las filas que hay
+while cont < f+1:
+    row_labels.append(cont) #Agregar la fila numero x al array
+    cont = cont+1
+column_labels = ['C1','C2','C3']
+"""#Mosrtar el arreglo
+for row in a:
+    for elem in row:
+        print(elem,end='\t')
+    print()
+"""
+#Mostrar el arreglo con columnas numero de fila
+df = pd.DataFrame(a,columns=column_labels,index=row_labels)
+print(df)
