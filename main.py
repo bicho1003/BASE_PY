7fb019776a523fc3f6800166b8b0daa50f6701d8
Rafael Alexander Peña Barragan <tonma1003@gmail.com>
2021-08-22 19:18:20 -0500

    Clase usuario y info a listas

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ import mymodule
 
 def main():
     print("Hola mundo")
-    usuario = mymodule.Usuario()
-    usuario.Clientadd()
+    persona1 = mymodule.Usuario("1","Daniel","Alberto","Ayala","Dominguez","328092489023")
+    print ("\n" + str(persona1) + "\n")
+    print(persona1.tostr())
     rect = mymodule.Rectangulo()
     rect.area()
 
--- mymodule.py
+++ mymodule.py
@@ -7,14 +7,37 @@ class Rectangulo:
     def area(self):
         print("El area es: " + str(self.b*self.h))
 
-class Usuario:
+class Usuario():
+    """Clase Principal usuario"""
+    
+    def __init__(self,idempleado,pnombre,snombre,papellido,sapellido,rfc):
+        self.idempleado = idempleado
+        self.Pnombre = pnombre
+        self.Snombre = snombre
+        self.Papellido = papellido
+        self.Sapellido = sapellido
+        self.Rfc = rfc
+
+    def __str__(self):
+        """Devuelve una cadena representativa de Persona"""
+        return "%s, %s %s, %s, %s, %s." % (
+            self.idempleado, self.Pnombre, self.Snombre, 
+            self.Papellido, self.Papellido,self.Rfc)
+    
+    def tostr(self):
+        objstr = [self.idempleado, self.Pnombre, self.Snombre, 
+            self.Papellido, self.Papellido,self.Rfc]
+        return objstr
+    
+    """" Constructor de la clase admin
     def __init__(self,see,add,delete,modify,list):
         self.see = see
         self.add = add
         self.delete = delete
         self.modify = modify
         self.list = list
-    
+    """
+    """  Funciones de Clase ADministrador y Gerente
     def Clientadd(self):
         if self.add == True:
             print("Usted puede agregar lo que quiera")
@@ -38,12 +61,12 @@ class Usuario:
             print("Usted puede enlistar lo que quiera")
         else:
             print("Usted no puede enlistar nada")
-
-class Admin(Usuario(True,True,True,True,True)):
+    """
+"""class Admin(Usuario(True,True,True,True,True)):
     def whoami():
         print("Soy un admin")
 
 class Gerente(Usuario(False,True,True,True,True)):
     def whoami():
          print("Soy un Gerente")
-
+"""
