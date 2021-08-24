c84169fe8603b9907bdf7398fb7a9a6ef4f697f4
ImJumo <daniel.ayala621@gmail.com>
2021-08-23 19:52:11 -0500

    Cometarios

--- SysFunc.py
+++ SysFunc.py
@@ -1,5 +1,8 @@
+#from FuncionAgregar import FunAd
+
 def SeeClients (verifi):
     if verifi == True:
+        #FunAd()
         print("Usted puede ver la lista de los clientes")
     else:
         print("Usted no puede ver la lista de los clientes")
